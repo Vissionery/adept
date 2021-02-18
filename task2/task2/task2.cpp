6408647c58e03c4369428eb1e45a1aad5478a867
Vissionery <79271202+Vissionery@users.noreply.github.com>
2021-02-18 22:58:40 +0300

    /laba1

---
+++ task2/task2/task2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+double f(double x, double y)
+{
+	y = (sin(2 * x) + sin(5 * x) - sin(3 * x)) / (cos(x) - cos(3 * x) + cos(5 * x));
+	return y;
+}
+void main()
+{
+	double a, b, c;
+	a = 1;c = 0;
+	setlocale(LC_ALL, "Rus");
+	cout << "Результат функции при х=1!" << endl;
+	cout << f(a, c) << endl;
+	cout << "Введите число!" << endl;
+	cin >> b;
+	cout << "Резултат функции при введённом числе!" << endl;
+	cout << f(b, c) << endl;
+}
