c36b2f6f16e5a42ab7f4c048550df91485831b5c
Vissionery <79271202+Vissionery@users.noreply.github.com>
2021-02-27 20:42:30 +0300

    task2

---
+++ task2/task2/task2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+double f(double x)
+{
+	if (x <= 3)
+		return pow(x,2) - 3 * x + 9;
+	else return 1 / (pow(x,3) + 6);
+}
+int main()
+{
+	double x,result;
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите число!" << endl;
+	cin >> x;
+	result = f(x);
+	cout << "Результат функции="<< endl;
+	cout << result << endl;
+}
+
+
